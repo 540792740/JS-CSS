f33083be5de722f7b9d6b8ab586603d043cd0c2b
Peter <peterwangjiawei@gmail.com>
2019-09-26 19:02:22 -0400

    JS

---
+++ JS_AJAX_PHP_courese/1_011_jQuery_savetime.js
@@ -0,0 +1,21 @@
+//JS jQuery
+
+var myNumber = 0
+// $(document).ready(function(){
+$(function() {
+    // $(".col-md-3").hover(function () {
+        // $(this).hide();
+        // myNumber++;
+        // $('output').html('clicked me ' + myNumber);
+        // $('*').toggle();
+        $(".col-md-3").on({
+            mouseenter: function () {
+                $(this).css("background-color", "#0000ff");
+            },
+            mouseleave: function () {
+                $(this).css("background-color", "#ff0000");
+            }
+
+        });
+    })
+
