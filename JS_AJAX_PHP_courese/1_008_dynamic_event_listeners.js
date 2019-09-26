9be118969878980ca6a83fef147ba94d0234a2f2
Peter <peterwangjiawei@gmail.com>
2019-09-26 17:45:08 -0400

    JS

--- JS_AJAX_PHP_courese/1_008_dynamic_event_listeners.js
+++ JS_AJAX_PHP_courese/1_008_dynamic_event_listeners.js
@@ -1,32 +1,38 @@
 //js doc
-
+/*
 // document.body.innerHTML = 'Hello world.'
 // document.title = 'hello'
-var myOutput = document.getElementsByTagName('h1');
 console.log(myOutput)
-myOutput.innerHTML = "Test";
-/*
+myOutput[1].onclick = function(){myFunction()};
+
 var myOutput = document.getElementById('output');
 var myButton = document.getElementById('myButton');
 console.log(this.myButton);
+
 
 myButton.innerHTML = 'Something Diff'
 
 myButton.onclick = function(){myFunction()};
 ///Working with Variables
+
+
 var myString = '1010';
-var myNumber = 0;
+
 var myBoolean = false;
 var myArray = ['hello 2', 100];
 var myObject = {name:'peter', sex: 'male'};
 
 var output = 'Nothing';
 var output = 'Nothing <BR> next line';
+ */
+var myNumber = 0;
 function myFunction(){
     myNumber++;
     document.getElementById('output').innerHTML = 'clicked me'+ myNumber;
 }
 
-myOutput.innerHTML = output;
+var myClass = document.getElementsByClassName('col-md-3');
 
- */
+for (i = 0; i < myClass.length; i++){
+    myClass[i].addEventListener("click", myFunction);
+}
